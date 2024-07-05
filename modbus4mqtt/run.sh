6f592d7206b158ea9b4264b4cab3d834ff774e9b
Jesse Kleemann <j.kleemann@tu-bs.de>
2024-07-05 16:03:33 +0200

    set config path

--- modbus4mqtt/run.sh
+++ modbus4mqtt/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-modbus4mqtt $(python -c "import json; opt=json.load(open('/data/options.json')); print(f\"--hostname {opt['mqtt_hostname']} --username {opt['mqtt_user']} --password {opt['mqtt_pass']} --config {opt['config_name']}\")")
+modbus4mqtt $(python -c "import json; opt=json.load(open('/data/options.json')); print(f\"--hostname {opt['mqtt_hostname']} --username {opt['mqtt_user']} --password {opt['mqtt_pass']} --config /config/{opt['config_name']}\")")
