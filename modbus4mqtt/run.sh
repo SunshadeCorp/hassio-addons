73467b670b600a2a947bcd33b0b55f7378018883
Jesse Kleemann <j.kleemann@tu-bs.de>
2024-07-05 15:33:32 +0200

    fix wrong container

--- modbus4mqtt/run.sh
+++ modbus4mqtt/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 sleep 5d
-python /modbus4mqtt/modbus4mqtt/modbus4mqtt.py --hostname mosquitto --username user --password pass --config /config/sungrow_sh10rt.yaml
+modbus4mqtt --hostname mosquitto --username user --password pass --config /config/sungrow_sh10rt.yaml
