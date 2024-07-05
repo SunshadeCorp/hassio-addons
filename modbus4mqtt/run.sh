75ef1df35e8a16b415207d7a3e0c45e464dceceb
Jesse Kleemann <j.kleemann@tu-bs.de>
2024-07-05 15:06:50 +0200

    fix path

--- modbus4mqtt/run.sh
+++ modbus4mqtt/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python /modbus4mqtt/modbus4mqtt.py --hostname mosquitto --username user --password pass --config /config/sungrow_sh10rt.yaml
+python /modbus4mqtt/modbus4mqtt/modbus4mqtt.py --hostname mosquitto --username user --password pass --config /config/sungrow_sh10rt.yaml
