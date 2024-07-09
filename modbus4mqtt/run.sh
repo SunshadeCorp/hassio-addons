b729749fb8f30979c6ae498c895d9c28e75ccc88
Jesse Kleemann <j.kleemann@tu-bs.de>
2024-07-09 15:25:21 +0200

    use sh instead of bash

--- modbus4mqtt/run.sh
+++ modbus4mqtt/run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 modbus4mqtt $(python -c "import json; opt=json.load(open('/data/options.json')); print(f\"--hostname {opt['mqtt_hostname']} --username {opt['mqtt_user']} --password {opt['mqtt_pass']} --config /config/{opt['config_name']}\")")
